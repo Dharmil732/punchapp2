bea811be6c4a673af40019b3c5ef85850900a227
Dharmil732 <81030524+Dharmil732@users.noreply.github.com>
2025-11-07 13:10:47 -0700

    Fix export statement for getClientSupabase function

--- punchapp/lib/authClient.ts
+++ punchapp/lib/authClient.ts
@@ -1,6 +1,7 @@
 "use client";
 import { createBrowserClient } from "@supabase/auth-helpers-nextjs";
-export function getClientSupabase(){
+
+export function getClientSupabase() {
   return createBrowserClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
